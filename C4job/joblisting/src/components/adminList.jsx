245d74e847cedf86a9c8f4e56ab0127bd56d5b72
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-01-17 16:49:51 +0530

    up

--- C4job/joblisting/src/components/adminList.jsx
+++ C4job/joblisting/src/components/adminList.jsx
@@ -3,12 +3,22 @@ import { useEffect, useState } from "react";
 
 export const AdminList = () => {
   const jobs = useSelector((state) => state.jobs);
+  console.log(jobs);
   return (
     <>
       <hr />
-      {jobs.map((e) => {
-        <div>{e}</div>;
-      })}
+      {jobs.map((e) => (
+        <div>
+          <h2>{e.company}</h2>
+          <h4>{e.title}</h4>
+          <p>{e.description}</p>
+          <p>{e.locaton}</p>
+          <div>
+            Salary {e.min} -- {e.max}
+          </div>
+          <hr />
+        </div>
+      ))}
     </>
   );
 };
